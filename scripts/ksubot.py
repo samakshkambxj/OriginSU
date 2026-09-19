81988d87f0d2ceac63b6faabaae604da964644cd
samakshkambxj <samakshk4@gmail.com>
2026-09-19 23:28:14 +0530

    fix(ci): point Telegram debug-build link at OriginSU repo

--- scripts/ksubot.py
+++ scripts/ksubot.py
@@ -80,7 +80,7 @@ Branch: {branch}
 </pre>
 {commit_line}
 <a href="{run_url}">Workflow run</a>
-<a href="https://nightly.link/ReSukiSU/ReSukiSU/workflows/build-manager/main/Manager-debug.zip">Get latest main Debug build</a>
+<a href="https://nightly.link/samakshkambxj/OriginSU/workflows/build-manager/main/Manager-debug.zip">Get latest main Debug build</a>
 """.strip()
 MAIN_UPDATED_MSG ="""
 main branch updated, manager in there may outdated 
