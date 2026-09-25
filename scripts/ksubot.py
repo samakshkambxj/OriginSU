3fc3cad332a65a36db77afd8e1d4dde2baf8a076
YC酱luyancib <luyancib@qq.com>
2026-09-25 13:49:44 +0800

    ksubot: split commit with `------`

--- scripts/ksubot.py
+++ scripts/ksubot.py
@@ -33,7 +33,7 @@ try:
             msg = commit['message'].strip()
             if len(msg) > 200:
                 msg = msg[:197] + '...'
-            msg += ' by ' + commit['author']['username']
+            msg += ' by ' + commit['author']['username'] + '\n------' if i > 1 else ''
             if len(msg) + 1 + len(commit_message) > 600:
                 commit_message = f'{commit_message}\n(other {i} commits)'
                 break
